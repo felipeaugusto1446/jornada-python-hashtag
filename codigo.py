2daf7d687655027c0666eea714957e21a23f7799
felipeaugusto1446 <felipeaugusto@makasi.com.br>
2026-01-13 10:55:07 -0300

    ajustando os tipos de dados que serao inserido, evitando a insercao de observacoes vazias e concluindo a aula

--- codigo.py
+++ codigo.py
@@ -4,7 +4,6 @@ import pandas
 
 pyautogui.PAUSE = 1
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
-
 pyautogui.press("win")
 pyautogui.write("opera")
 pyautogui.press("enter")
@@ -24,29 +23,37 @@ pyautogui.press("enter")
 tabela = pandas.read_csv("material/produtos.csv")
 
 for linha in tabela.index:
-    # clicar no campo de código
+    
     pyautogui.click(x=946, y=240)
     # pegar da tabela o valor do campo que a gente quer preencher
-    codigo = tabela.loc[linha, "codigo"]
-    # preencher o campo
-    pyautogui.write(str(codigo))
-    # passar para o proximo campo
+    codigo = str(tabela.loc[linha, "codigo"])
+    pyautogui.write(codigo)
+    
     pyautogui.press("tab")
-    # preencher o campo
-    pyautogui.write(str(tabela.loc[linha, "marca"]))
+
+    marca = str(tabela.loc[linha, "marca"])
+    pyautogui.write(marca)
     pyautogui.press("tab")
     
-    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    tipo = str(tabela.loc[linha, "tipo"])
+    pyautogui.write(tipo)
     pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(categoria)
     pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(preco_unitario)
     pyautogui.press("tab")
-    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)
     pyautogui.press("tab")
-    obs = tabela.loc[linha, "obs"]
+    
+    obs = str(tabela.loc[linha, "obs"])
     if obs != "nan":
-        pyautogui.write(str(tabela.loc[linha, "obs"]))
+        pyautogui.write(obs)
         
     pyautogui.press("tab")
     pyautogui.press("enter") # cadastra o produto (botao enviar)
