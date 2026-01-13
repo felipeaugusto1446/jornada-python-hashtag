80faa5bcefa6f49a59994a219a8f228d071c3ffb
felipeaugusto1446 <felipeaugusto@makasi.com.br>
2026-01-12 21:53:28 -0300

    pegando dados da tabela e inserindo no site com lógica for

--- codigo.py
+++ codigo.py
@@ -1,5 +1,6 @@
 import pyautogui
 import time
+import pandas
 
 pyautogui.PAUSE = 1
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
@@ -10,14 +11,47 @@ pyautogui.press("enter")
 
 pyautogui.write(link)
 pyautogui.press("enter")
-
+    
 pyautogui.click(x=768, y=358)
 pyautogui.write("teste2393@gmail.com")
 
-pyautogui.click(x=817, y=456)
+pyautogui.press("tab")
 pyautogui.write("senhaço123")
 
 pyautogui.press("tab")
 pyautogui.press("enter")
 
-time.sleep(3)
+tabela = pandas.read_csv("material/produtos.csv")
+
+for linha in tabela.index:
+    # clicar no campo de código
+    pyautogui.click(x=946, y=240)
+    # pegar da tabela o valor do campo que a gente quer preencher
+    codigo = tabela.loc[linha, "codigo"]
+    # preencher o campo
+    pyautogui.write(str(codigo))
+    # passar para o proximo campo
+    pyautogui.press("tab")
+    # preencher o campo
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    if obs != "nan":
+        pyautogui.write(str(tabela.loc[linha, "obs"]))
+        
+    pyautogui.press("tab")
+    pyautogui.press("enter") # cadastra o produto (botao enviar)
+    # dar scroll de tudo pra cima
+    pyautogui.scroll(5000)
+    
+
+time.sleep(2)
